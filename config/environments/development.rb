6d64d6d5bf59b27d55f1bba91e86b072bb7e5fb5
Andrew Hood <andrewhood125@gmail.com>
2024-10-16 13:41:39 -0700

    Remove unused files & directories
    Everything you need and nothing you don't.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,9 +40,6 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
-  # Highlight code that enqueued background job in logs.
-  config.active_job.verbose_enqueue_logs = true
-
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
--- config/importmap.rb
+++
@@ -1,6 +0,0 @@
-# Pin npm packages by running ./bin/importmap
-
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "radar-sdk-js" # @4.4.3
-pin "maplibre-gl" # @4.7.1
